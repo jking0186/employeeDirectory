79b191f32adc2f973de8bac7a8106be1e889180a
Joe King <jking0186@gmail.com>
2020-07-21 16:29:27 -0500

    page layout

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron bg-dark">
+            <h1 className="text-center text-white">Employee Directory</h1>
+            <h5 className="text-center text-white">Filter by heading or use search bar to narrow results.</h5>
+        </div>
+    );
+}
+
+export default Jumbotron;
