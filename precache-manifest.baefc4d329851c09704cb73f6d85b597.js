5cd6fc3c6e113f3c9f2dcc954f99c3ed070e397b
Joe King <jking0186@gmail.com>
2020-07-21 16:32:41 -0500

    Updates

---
+++ precache-manifest.baefc4d329851c09704cb73f6d85b597.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79ff8e7839c10f24dc04a71905d4cbad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e0788b75b7017919c40",
+    "url": "/static/css/main.777f9db0.chunk.css"
+  },
+  {
+    "revision": "6eb7d0e48416b2f8eb36",
+    "url": "/static/js/2.cb2269ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cb2269ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e0788b75b7017919c40",
+    "url": "/static/js/main.68dfde4e.chunk.js"
+  },
+  {
+    "revision": "1a9355e74d03dc4c7068",
+    "url": "/static/js/runtime-main.e0225f3c.js"
+  }
+]);
