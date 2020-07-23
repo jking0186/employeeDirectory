bc4de96306cf830da5064ea8e27fe17008fc8bad
Joe King <jking0186@gmail.com>
2020-07-23 08:35:34 -0500

    organizing search

--- src/components/Searchbar/SearchBar.js
+++ src/components/Searchbar/SearchBar.js
@@ -5,7 +5,12 @@ function SearchBar() {
         <div className="container">
         <div className="row justify-content-center">
             <div className="col-lg-4 col-md-4 col-sm-6">
-                <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Employee Search"></input>
+                <input 
+                type="text" 
+                className="form-control" 
+                id="formGroupExampleInput" 
+                placeholder="Employee Search"
+                />
             </div>
         </div>
         </div>
